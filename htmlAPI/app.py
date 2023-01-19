de18fae1d5739300d30bd294c1bfed2c37dca6a8
ariclavel <a01706629@itesm.mx>
2023-01-19 15:16:51 -0400

    changes

--- htmlAPI/app.py
+++ htmlAPI/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
+
 app = Flask(__name__)
 
 #rendering the HTML page which has the button
@@ -6,8 +7,20 @@ app = Flask(__name__)
 def json():
     return render_template('main.html')
 
-@app.route("/forward/", methods=['POST'])
+@app.route("/forward/", methods=['POST','GET'])
 def move_forward():
+    if request.method == "GET":
+        forward_message = "Moving Forward..."
+        return render_template('info.html', titulo=forward_message)
+
     #Moving forward code
-    forward_message = "Moving Forward..."
-    return render_template('main.html', forward_message=forward_message);
+    if request.method == "POST":
+        req = request.form
+        print(req)
+        a = req.get("lien")
+        print(a)
+        url = "http://testphp.vulnweb.com/artists.php?artist=1"
+        attackSql(url)
+        return render_template("main.html", titulo=a)
+ 
+    
